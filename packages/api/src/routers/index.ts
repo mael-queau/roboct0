97a0362d0cd4ad75359237f015e8aa249aeca8b7
Maël Quéau <mael.queau99@gmail.com>
2024-05-23 15:10:57 +0200

    feat(api): twitch user access token router

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,3 +1,6 @@
 import { router } from "../trpc";
+import { twitchTokenRouter } from "./twitch/token";
 
-export const appRouter = router({});
+export const appRouter = router({
+  twitchToken: twitchTokenRouter,
+});
--- packages/api/src/routers/twitch/token.ts
+++ packages/api/src/routers/twitch/token.ts
@@ -1,11 +1,75 @@
 import { z } from "zod";
-import { getTwitchUserToken } from "../../controllers/twitchUserToken";
+import {
+  createTwitchUserToken,
+  deleteTwitchUserToken,
+  getTwitchUserToken,
+  listTwitchUserTokens,
+  updateTwitchUserToken,
+} from "../../controllers/twitchUserToken";
 import { publicProcedure, router } from "../../trpc";
 
-const twitchTokenRouter = router({
+export const twitchTokenRouter = router({
+  listTokens: publicProcedure.query(async () => {
+    return listTwitchUserTokens();
+  }),
+  getBotToken: publicProcedure.query(async () => {
+    return getTwitchUserToken();
+  }),
   getToken: publicProcedure
     .input(z.object({ userId: z.string() }))
-    .query(async ({ input }) => {
-      return getTwitchUserToken(input.userId);
+    .query(async (opts) => {
+      const { userId } = opts.input;
+
+      return getTwitchUserToken(userId);
+    }),
+  createToken: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        accessToken: z.string(),
+        refreshToken: z.string().nullable(),
+        expiresIn: z.number().nullable(),
+        obtainedAt: z.date(),
+      })
+    )
+    .mutation(async (opts) => {
+      const { userId, accessToken, refreshToken, expiresIn, obtainedAt } =
+        opts.input;
+
+      return createTwitchUserToken({
+        userId,
+        accessToken,
+        refreshToken,
+        expiresIn,
+        obtainedAt,
+      });
+    }),
+  updateToken: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        accessToken: z.string(),
+        refreshToken: z.string().nullable(),
+        expiresIn: z.number().nullable(),
+        obtainedAt: z.date(),
+      })
+    )
+    .mutation(async (opts) => {
+      const { userId, accessToken, refreshToken, expiresIn, obtainedAt } =
+        opts.input;
+
+      return updateTwitchUserToken(userId, {
+        accessToken,
+        refreshToken,
+        expiresIn,
+        obtainedAt,
+      });
+    }),
+  deleteToken: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .mutation(async (opts) => {
+      const { userId } = opts.input;
+
+      return deleteTwitchUserToken(userId);
     }),
 });
